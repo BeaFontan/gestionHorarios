065938260f41ad9d3c26a8b04e762b2b36fb2dfa
Efrén Corzón Vázquez <efcovaz@gmail.com>
2025-01-29 18:53:17 +0100

    creacion conexion bbdd

---
+++ GestionHorarios/www/functions/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "dbXdebug";
+$user = "root";
+$pass = "root";
+$bbdd = "Gestion_Horarios";
+
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$bbdd", $user, $pass);
+
+    echo "conexion ok!";
+} catch (PDOException $e) {
+    echo "erro na conexion " . $e->getMessage();
+}
---
+++ GestionHorarios/www/pages/login.php
@@ -0,0 +1,5 @@
+<?php
+
+//include_once "conexion.php";
+
+echo "hola";
