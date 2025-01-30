fd3fa13079cc54a97aa7d0d27e38c8aff12916e6
A23BeatrizFP <a23beatrizfp@a24eql01.sanclemente.local>
2025-01-30 17:07:06 +0100

    login

--- GestionHorarios/www/pages/login.php
+++ GestionHorarios/www/pages/login.php
@@ -1,6 +1,27 @@
 <?php
 
-include "../functions/conexion.php";
 
 
-echo "hola bea";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <h1>Timetable IES San Clemente</h1>
+
+    <form action="/prueba.php" method="get">
+        <input type="text" name="txtUser" id= "txtUser" placeholder="Usuario">
+        <input type="password" name="txtPass" id="txtPass" placeholder="Contrasinal">
+        <button type="submit" name="btnLogin" id="btnLogin">Entrar</button>
+    </form>
+
+    <a href="#">¿Esqueceches a túa contrasinal?</a>
+    
+</body>
+</html>
