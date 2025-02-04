e824b933f96f3683255589ed6a9a06e723de82a0
Efrén Corzón Vázquez <efcovaz@gmail.com>
2025-02-04 11:50:37 +0100

    css login

--- GestionHorarios/www/pages/login.php
+++ GestionHorarios/www/pages/login.php
@@ -1,27 +1,32 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
 
-    <h1>Timetable IES San Clemente</h1>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" href="../pages/css/style.css" />
+</head>
+
+<body>
+    <h1>
+        <span class="raya">Timetable</span> <span class="negrita">IES</span> San
+        Clemente
+    </h1>
+
+    <div class="circle"></div>
 
     <form action="/prueba.php" method="get">
-        <input type="text" name="txtUser" id= "txtUser" placeholder="Usuario">
-        <input type="password" name="txtPass" id="txtPass" placeholder="Contrasinal">
+        <input type="text" name="txtUser" id="txtUser" placeholder="Usuario" />
+        <input
+            type="password"
+            name="txtPass"
+            id="txtPass"
+            placeholder="Contrasinal" />
         <button type="submit" name="btnLogin" id="btnLogin">Entrar</button>
     </form>
 
     <a href="#">¿Esqueceches a túa contrasinal?</a>
-    
 </body>
+
 </html>
